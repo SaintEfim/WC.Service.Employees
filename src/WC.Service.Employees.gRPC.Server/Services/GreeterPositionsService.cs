f53e44447e1e17e86df3a5b72f3acbe50af92c2f
SaintEfim <saintefim2@gmail.com>
2024-07-22 10:56:11 +0300

    update dependencies

--- src/WC.Service.Employees.gRPC.Server/Services/GreeterPositionsService.cs
+++ src/WC.Service.Employees.gRPC.Server/Services/GreeterPositionsService.cs
@@ -19,7 +19,10 @@ public class GreeterPositionsService : GreeterPositions.GreeterPositionsBase
     {
         var position = await _provider.GetOneByName(request.Position.Name, context.CancellationToken);
 
-        if (position == null) throw new RpcException(new Status(StatusCode.NotFound, "Position not found"));
+        if (position == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Position not found"));
+        }
 
         return new GetOneByNamePositionResponse
         {
