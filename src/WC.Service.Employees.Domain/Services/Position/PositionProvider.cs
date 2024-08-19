dc3ccf6d9ae8b843b6c206c3b0d526f3b6cbddfe
SaintEfim <saintefim2@gmail.com>
2024-08-19 15:12:44 +0300

    removed un using method

--- src/WC.Service.Employees.Domain/Services/Position/PositionProvider.cs
+++ src/WC.Service.Employees.Domain/Services/Position/PositionProvider.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
-using WC.Library.Data.Services;
 using WC.Library.Domain.Services;
 using WC.Service.Employees.Data.Models;
 using WC.Service.Employees.Data.Repositories;
@@ -18,16 +17,5 @@ public class PositionProvider
         IPositionRepository repository)
         : base(mapper, logger, repository)
     {
-    }
-
-    public async Task<PositionModel?> GetOneByName(
-        string positionName,
-        IWcTransaction? transaction = default,
-        CancellationToken cancellationToken = default)
-    {
-        var positions = await Repository.Get(transaction: transaction, cancellationToken: cancellationToken);
-        var position = positions.SingleOrDefault(x => x.Name == positionName);
-
-        return Mapper.Map<PositionModel>(position);
     }
 }
