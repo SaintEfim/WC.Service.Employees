b5991cc807e24c3ba752a080c91623c23931e779
SaintEfim <saintefim2@gmai.com>
2024-09-15 16:03:33 +0300

    added error handler

---
+++ src/WC.Service.Employees.Data.PostgreSql/Migrations/20240915100613_Employee_fixed_configuration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WC.Service.Employees.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Employee_fixed_configuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_PositionId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Colleagues_EmployeeId_ColleagueEmployeeId",
+                table: "Colleagues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PositionId",
+                table: "Employees",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colleagues_EmployeeId",
+                table: "Colleagues",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_PositionId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Colleagues_EmployeeId",
+                table: "Colleagues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PositionId",
+                table: "Employees",
+                column: "PositionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colleagues_EmployeeId_ColleagueEmployeeId",
+                table: "Colleagues",
+                columns: new[] { "EmployeeId", "ColleagueEmployeeId" },
+                unique: true);
+        }
+    }
+}
