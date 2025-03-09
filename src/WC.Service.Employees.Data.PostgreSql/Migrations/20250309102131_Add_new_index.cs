9ac57f9e885f272c00b12240ef5e65b85edd45a8
SaintEfim <saintefim2@gmai.com>
2025-03-09 13:22:39 +0300

    update migration

---
+++ src/WC.Service.Employees.Data.PostgreSql/Migrations/20250309102131_Add_new_index.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WC.Service.Employees.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_new_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Colleagues_EmployeeId",
+                table: "Colleagues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colleagues_EmployeeId_ColleagueEmployeeId",
+                table: "Colleagues",
+                columns: new[] { "EmployeeId", "ColleagueEmployeeId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Colleagues_EmployeeId_ColleagueEmployeeId",
+                table: "Colleagues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colleagues_EmployeeId",
+                table: "Colleagues",
+                column: "EmployeeId");
+        }
+    }
+}
